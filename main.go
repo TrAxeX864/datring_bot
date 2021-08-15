6ffb544609ab0b5a6859456f23ca07a13a56f7c6
traxex864 <traxex864@gmail.com>
2021-08-16 02:14:00 +0700

    Подключена Telegram библиотека. Создан простой telegram bot

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	bot, err := tgbotapi.NewBotAPI("568578863:AAHQOl3Mj-IYi-ed0BXbQ-mvXa2JzjRNOTk")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
 }
